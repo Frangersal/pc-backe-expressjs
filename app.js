4ddac2171cee80b426f0afc98b2e9c02e0d572c0
frangersal <frangersal@outlook.com>
2025-08-25 22:01:41 -0600

    Clase 4 Creamos 2 rutas, uno para usuarios y otro para busqueda con termino y categoria recibidos por la URL

--- app.js
+++ app.js
@@ -4,9 +4,15 @@
 
 require('dotenv').config();
 // Importa el módulo express
+// Se importa body-parser para poder manejar datos enviados en el cuerpo de las peticiones (POST, PUT, etc.)
 const express = require('express');
+// Importar modulo body-parser
+const bodyParser = require('body-parser');
 // Crea una instancia de la aplicación express
 const app = express();
+// Se configura body-parser para aceptar JSON y datos de formularios
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
 
 // Define el puerto a usar, tomando el valor de la variable de entorno PORT o 3000 por defecto
 const PORT = process.env.PORT || 3000;
@@ -20,7 +26,24 @@ app.get('/', (req, res) => {
     <p>Esto es una app node.js con express.js </p>
     <p>Corre en el puerto: ${PORT} </p>
     `
+// Ruta para mostrar información de un usuario según su ID recibido por parámetro en la URL
     );
+});
+
+app.get('/users/:id',(req, res)=>{
+// Ruta para realizar búsquedas usando parámetros de consulta (query params)
+    const userId = req.params.id;
+    res.send(`Mostrar información del usuario con ID: ${userId}`)
+});
+app.get('/search', (req,res)=>{
+// Ruta que permite realizar búsquedas utilizando parámetros de consulta (query string) recibidos en la URL
+    const terms = req.query.termino || 'No especificado';
+    const category = req.query.categoria || 'Todas';
+    res.send(`
+        <h2>Resultados de Busqueda:</h2>
+        <p>Término: ${terms}</p>
+        <p>Categoría: ${category}</p>
+        `)
 });
 
 // Inicia el servidor y lo pone a escuchar en el puerto definido
