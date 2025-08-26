d075b0827120c577b5c7036eeb1c93c345a498cb
frangersal <frangersal@outlook.com>
2025-08-25 21:28:59 -0600

    Clase 3 configure variable de entorno PORT con dotenv, tambien cambie package.json de modo que facilite algunos scripts como dev y start

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
+// Carga las variables de entorno desde el archivo .en
+// Para que nuestra aplicación pueda leer las variables definidas en un archivo .env, necesitamos instalar el paquete dotenv:
+// npm install dotenv
+
+require('dotenv').config();
+// Importa el módulo express
 const express = require('express');
+// Crea una instancia de la aplicación express
 const app = express();
 
+// Define el puerto a usar, tomando el valor de la variable de entorno PORT o 3000 por defecto
 const PORT = process.env.PORT || 3000;
+console.log(PORT)
 
+// Define la ruta principal y la respuesta que se envía al acceder a '/'
 app.get('/', (req, res) => {
     res.send(
     `
-    <h1>Curso Express.js no</h1>
+    <h1>Curso Express.js</h1>
     <p>Esto es una app node.js con express.js </p>
     <p>Corre en el puerto: ${PORT} </p>
     `
     );
 });
 
+// Inicia el servidor y lo pone a escuchar en el puerto definido
 app.listen(PORT, () => {
-    console.log('Nuestra aplicación está funcionando ${PORT}');
+    console.log(`Servidor: http://localhost:${PORT}`);
 });
