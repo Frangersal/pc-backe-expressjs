6d0d613642eb5aadfd284e3b4c63dae7ee8237c4
frangersal <frangersal@outlook.com>
2025-08-25 19:46:15 -0600

    Clase 2 Gestión de Versiones y Configuración de Node.js con NVM

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send(
+    `
+    <h1>Curso Express.js no</h1>
+    <p>Esto es una app node.js con express.js </p>
+    <p>Corre en el puerto: ${PORT} </p>
+    `
+    );
 });
 
-app.listen(port, () => {
-    console.log('Nuestra aplicación está funcionando');
+app.listen(PORT, () => {
+    console.log('Nuestra aplicación está funcionando ${PORT}');
 });
