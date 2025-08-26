aabd631aa5bb4748f718704871ec46ffd30ebcac
frangersal <frangersal@outlook.com>
2025-08-25 22:44:02 -0600

    Clase 5 Punto de entrada para procesar formularios, esta vez accederemos a los datos por el BODY de la peticion, y un segundo punto de entrada donde verifica si se enviaron datos

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ app.get('/users/:id',(req, res)=>{
 });
 app.get('/search', (req,res)=>{
 // Ruta que permite realizar búsquedas utilizando parámetros de consulta (query string) recibidos en la URL
-    const terms = req.query.termino || 'No especificado';
+// Ej. http://localhost:3005/search?termino=expressjs&categoria=nodejs
+const terms = req.query.termino || 'No especificado';
     const category = req.query.categoria || 'Todas';
     res.send(`
         <h2>Resultados de Busqueda:</h2>
@@ -45,6 +46,38 @@ app.get('/search', (req,res)=>{
         <p>Categoría: ${category}</p>
         `)
 });
+
+//Ruta para pocesar formularios
+app.post('/form',(req, res)=> {
+    //Accederemos a los datos en el BODY de la peticion
+    // Si probamos en URL no podremos verlo en el navegador 
+    // Usaremos Postman
+    const name = req.body.nombre || 'Anónimo'
+    const email = req.body.email || 'No proporcionado'
+    res.json({
+        message: 'Datos recibidos', 
+        data: {
+            name, 
+            email
+        }
+    })
+
+});
+// Ruta para recibir datos JSON mediante una petición POST y responder con los datos recibidos o un error si no se envían datos
+app.post('/api/data', (req, res)=>{
+    const data = req.body;
+    if(!data || Object.keys(data).length === 0){
+        return res.status(400).json({error: 'No se recibieron datos'})
+    }
+
+    res.status(201).json({
+        message:'Datos JSON recibidos',
+        data
+    })
+});
+
+
+
 
 // Inicia el servidor y lo pone a escuchar en el puerto definido
 app.listen(PORT, () => {
